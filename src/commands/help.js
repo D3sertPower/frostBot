35aeb41a1b826d48f1bc3be537a635bb6bc202a8
unknown <mateusinho230408@gmail.com>
2026-09-04 20:06:36 -0300

    refactor(help): permission based help command returns

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,19 +1,26 @@
 const { pre } = require('../chat-format');
-
+const { checkPermission } = require('../permissions')
 module.exports = {
   name: 'help',
   aliases: ['h', 'ajuda'],
   description: 'Lists all commands and their properties.',
 
   run() {
+    const requesterSteamID = Array.from(arguments).at(-1)
     const { COMMANDS } = require('../message-handler');
-
     const lines = [
       '📚 FROSTBOAT COMMAND DECK ❄️',
       '━━━━━━━━━━━━━━━━━━━━━━━━━━',
     ];
 
     for (const command of COMMANDS.values()) {
+
+      // A primeira perm é sempre a mais importante, condição para visualizar o comando no help.
+      const perm = command?.perms?.[0]
+      if (!checkPermission(requesterSteamID, perm)) {
+      continue
+      }
+
       const usage = command.args?.length
         ? `!${command.name} ${command.args.join(' ')}`
         : `!${command.name}`;
